b6427f86d0934bf4e18f35256310b718744b6e03
Jon Short <JonShort@users.noreply.github.com>
2017-05-20 22:01:37 +0100

    Added ability for user to input

--- src/components/title/Title.jsx
+++ src/components/title/Title.jsx
@@ -1,22 +1,23 @@
 // @flow
 import React from 'react';
 
+import '../../styles/containers.css'
 import './title.css';
 
 const Title = (
   props: {
-    title?: string,
+    title: string,
     onClick: () => mixed,
   }) => {
 
   return (
     <div className="wrapper">
-      <div className="container" onClick={props.onClick}>
+      <div className="click-area" onClick={props.onClick}>
         <span className="small white">the</span>
         <h1 className="title white">
-          {props.title ? props.title : "Aesthetic"}
+          {props.title}
         </h1>
-        {props.title && <span className="small white right">aesthetic</span>}
+        <span className="small white right">aesthetic</span>
       </div>
     </div>
   );
