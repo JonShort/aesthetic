8666e5033a09ffa37e811b2a6c45389bc87f010d
Jonathan Short <jonshort@Jonathans-MacBook-Pro-2.local>
2017-08-07 13:28:28 +0100

    Added flow to Image component

--- src/components/image/Image.jsx
+++ src/components/image/Image.jsx
@@ -1,7 +1,22 @@
+// @flow
 import React from 'react';
 
-const Image = ({alt, height, src, width}) => (
-    <img alt={alt} src={src} style={{maxHeight: height, maxWidth: width}} />
+const Image = (
+  props: {
+    alt: string,
+    height: string,
+    src: string,
+    width: string
+  }
+  ) => (
+  <img
+    alt={props.alt}
+    src={props.src}
+    style={{
+      maxHeight: props.height,
+      maxWidth: props.width
+    }}
+  />
 );
 
 export default Image;
